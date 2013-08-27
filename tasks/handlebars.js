9480679a56a1b87abf6aaea8ead63dd9874c752b
perfectworks <perfectworks@gmail.com>
2013-08-27 12:18:47 +0800

    Split compile logical to lib

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -6,21 +6,10 @@
  * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
  */
 
-/*jslint node: true, vars: true, nomen: true, indent: 4, plusplus: true, sloppy: true*/
+'use strict';
 
 module.exports = function (grunt) {
-    var path = require('path');
-    var handlebars = require('handlebars');
-
-    function expandFiles(param) {
-        if (grunt.file.expand) {
-            return grunt.file.expand({
-                filter: 'isFile'
-            }, param);
-        }
-
-        return grunt.file.expandFiles(param);
-    }
+    var compile = require('../lib/handlebars').compile;
 
     /**
      * Convert mustache template file to javascript script.
@@ -29,32 +18,14 @@ module.exports = function (grunt) {
         var content = grunt.file.read(filePath);
         var moduleName = filePath.replace(rootPath, '').replace(/^\//, '');
 
-        /*
-         * Calc out the className in `require` function
-         * xxx.handlebars -> ../util/Template
-         * a/xxx.handlebars -> ../../util/Template
-         * a/a/xxx.handlebars -> ../../../util/Template
-         */
-        var pathCount = moduleName.split('/').length - 1;
-        while (pathCount > 0) {
-            tmplClass = '../' + tmplClass;
-            pathCount -= 1;
-        }
-
-        var precompile = handlebars.precompile(content);
-
-        return 'define(function(require){' +
-            'var Template = require("' + tmplClass + '");' +
-            'return new Template("' + moduleName + '", ' + precompile + ');' +
-            '});';
+        return compile(moduleName, content, tmplClass);
     }
 
-    grunt.registerMultiTask('handlebars', 'Compile handlebars template to SeaJS module.', function (arg1, arg2) {
+    grunt.registerMultiTask('handlebars', 'Compile handlebars template to SeaJS module.', function () {
         var options = this.options();
 
         this.files.forEach(function (file) {
             var src = file.src[0];
-            var dest = file.dest;
 
             var result = generateTemplate(src, file.orig.cwd, options.templateModule);
 
